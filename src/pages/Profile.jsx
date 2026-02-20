246b3e41972e0ab9b2d89242553f090cff9d4c3a
rahullohchab5210 <rahullohchab5210@gmail.com>
2026-02-20 14:27:11 +0530

    path change

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import LogOut from "../components/LogOut";
 import Input from "../components/Input";
 
 function Profile() {
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
+    const [open, setOpen] = useState(false);
 
     useEffect(() => {
         const logged = JSON.parse(localStorage.getItem("loggedUser"));
@@ -17,34 +17,94 @@ function Profile() {
         }
     }, []);
 
+    const logout = () => {
+        localStorage.removeItem("loggedUser");
+        localStorage.removeItem("registeredUser");
+        navigate("/register");
+    };
 
     if (!user) return null;
 
     return (
-       
         <div>
-            <LogOut user={user} />
-            <div className=" w-full mx-auto mt-10 p-16 max-w-147.5  p-16 bg-white rounded-[15px]  shadow-[0px_10px_30px_0px_#0000000D] border border-[#E3E3E3]">
-                <div className="flex flex-col gap-[18px]"> 
+
+            <div className="bg-blue-600 text-white p-3 flex justify-between items-center">
+                <h2 className="text-xl font-bold">My Profile</h2>
+
+                <div className="relative">
+                    <div
+                        className="flex items-center gap-2 cursor-pointer"
+                        onClick={() => setOpen(!open)}
+                    >
+                        <img
+                            src={user.profilePicture}
+                            className="w-10 h-10 rounded-full object-cover"
+                            alt="profile"
+                        />
+                        <p className="font-semibold">
+                            {user.firstName.charAt(0).toUpperCase()}{" "}
+                            {user.lastName.charAt(0).toUpperCase()}
+                        </p>
+                    </div>
+
+                    {open && (
+                        <div className="absolute right-0 mt-2 bg-white shadow-lg text-black rounded-md p-2 w-32">
+                            <button
+                                className="w-full text-left px-2 py-1 hover:bg-gray-200 cursor-pointer"
+                                onClick={logout}
+                            >
+                                Logout
+                            </button>
+                        </div>
+                    )}
+                </div>
+            </div>
+ 
+            <div className="w-full mx-auto mt-10 p-16 max-w-147.5 bg-white rounded-[15px] shadow-[0px_10px_30px_0px_#0000000D] border border-[#E3E3E3]">
+                <div className="flex flex-col gap-[18px]">
                     <img
-                                src={user.profilePicture}
-                                className="w-32 h-32  rounded-full mx-auto object-cover"
-                            />
+                        src={user.profilePicture}
+                        alt="profile"
+                        className="w-32 h-32 rounded-full mx-auto object-cover"
+                    />
+
                     <div className="flex flex-row gap-[64px]">
-                        
-                    <div>
-                        <label className="font-semibold text-[20px] mb-[8px]">First Name</label>
-                            <Input type="text" name="firstName" placeholder="First Name" value={user.firstName}  className=" border-2 border-[#858585] rounded-[5px] text-[16px]" />
+                        <div>
+                            <label className="font-semibold text-[20px] mb-[8px]">
+                                First Name
+                            </label>
+                            <Input
+                                type="text"
+                                name="firstName"
+                                value={user.firstName}
+                                className="border-2 border-[#858585] rounded-[5px] text-[16px]"
+                            />
+                        </div>
+
+                        <div>
+                            <label className="font-semibold text-[20px] mb-[8px]">
+                                Last Name
+                            </label>
+                            <Input
+                                type="text"
+                                name="lastName"
+                                value={user.lastName}
+                                className="border-2 border-[#858585] rounded-[5px] text-[16px]"
+                            />
+                        </div>
                     </div>
+
                     <div>
-                        <label className="font-semibold text-[20px] mb-[8px]">Last Name</label>
-                        <Input type="text" name="lastName" placeholder="Last Name" value={user.lastName}  className="w-full p-2 border rounded-[5px]"/>
+                        <label className="font-semibold text-[20px] mb-[8px]">
+                            Email
+                        </label>
+                        <Input
+                            type="text"
+                            name="email"
+                            value={user.email}
+                            className="border-2 border-[#858585] rounded-[5px] text-[16px]"
+                        />
                     </div>
-                </div>
-                <div>
-                    <label className="font-semibold text-[20px] mb-[8px]">Email</label>
-                    <Input type="text" name="email" placeholder="Email" value={user.email} className="w-full p-2 border rounded-[5px] text-[16px]"/>
-                </div>
                 </div>
             </div>
         </div>
